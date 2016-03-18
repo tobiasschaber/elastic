89403268dc6f8e963eba18644a935ad1a9d68939
Tobias Schaber <tobias.schaber@codecentric.de>
2016-03-18 12:52:16 +0100

    adjusted finisher which restarts ELK for debian and rhel

--- installation/finish-install-elknode.sh
+++ installation/finish-install-elknode.sh
@@ -3,4 +3,19 @@
 # This script will be executed after the installation process of an
 # elasticsearch node has finished.
 
-sudo systemctl restart elasticsearch-es-01
+
+
+# for debian systems, use service
+if [ -f /etc/debian_version ]
+    then
+    sudo service elasticsearch-es-01 restart
+fi
+
+
+# for RHEL/CentOS systems, use systemctl
+if [ -f /etc/redhat-release ]
+    then
+        sudo systemctl restart elasticsearch-es-01
+fi
+
+
