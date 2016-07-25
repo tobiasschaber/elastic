eb2158f4b78755449ffce0401b4e498ccaf4de40
Tobias Schaber <tobias.schaber@codecentric.de>
2016-07-25 16:38:03 +0200

    improved starter scripts

--- tools/restoreSnapshotKibana.sh
+++ tools/restoreSnapshotKibana.sh
@@ -11,20 +11,64 @@ if [ -z "$1" ]
                 exit
 fi
 
-echo "restoring snapshot $1"
 
-# create the snapshot repository in elk
-curl -XPUT -s "http://10.0.3.131:9200/_snapshot/elk_backup" -d '{
+read -p "please enter the ELK client hostname (default: 10.0.3.131) :" hostname
+
+# check if user pushed [enter] for default value
+if [ -z "$hostname" ]
+		then
+		hostname="10.0.3.131"
+fi
+
+echo "saving snapshot under $1"
+
+
+
+
+# check if SSL is enabled
+if grep -q -e "\s*http.ssl:\s*true" hiera/common.yaml;
+then
+	elk_base_url="https://$hostname:9200"
+else
+	elk_base_url="http://$hostname:9200"
+fi
+
+
+
+# check if authentication is enabled
+authString=""
+if grep -q -e "\s*enable_authentication:\s*true" hiera/common.yaml;
+	then
+	echo "authentication is enabled"
+	elk_username=$(less hiera/common.yaml | grep -e "\ſ*[^\._]username" | cut -d : -f2 | tr -d ' ')
+	elk_password=$(less hiera/common.yaml | grep -e "\ſ*[^\._]password" | cut -d : -f2 | tr -d ' ')
+	authString="-u $elk_username:$elk_password"
+fi
+
+
+
+snapshotRepoCmd="curl -XPUT -s $authString \"$elk_base_url/_snapshot/elk_backup\" -d '{
       "type": "fs",
       "settings": {
           "location": "/tmp/elkinstalldir/snapshots/"
       }
-  }'
-
+  }'"
 
 # close the kibana index, restore it from snapshot, and reopen it
-curl -XPOST -s "http://10.0.3.131:9200/.kibana/_close"
-curl -XPOST -s "http://10.0.3.131:9200/_snapshot/elk_backup/$1/_restore"
-curl -XPOST -s "http://10.0.3.131:9200/.kibana/_open"
+closeKibanaIndexCmd="curl -XPOST -s $authString \"$elk_base_url/.kibana/_close\""
+restoreSnapshotCmd="curl -XPOST -s $authString \"$elk_base_url/_snapshot/elk_backup/$1/_restore\""
+reopenKibanaIndexCmd="curl -XPOST -s $authString \"$elk_base_url/.kibana/_open\""
+
+
+echo "closing kibana index..."
+eval $closeKibanaIndexCmd
+echo "restoring snapshot..."
+eval $restoreSnapshotCmd
+echo "reopening kibana index..."
+eval $reopenKibanaIndexCmd
+
+#curl -XPOST -u esadmin:esadmin  -s "http://10.0.3.131:9200/.kibana/_close"
+#curl -XPOST -u esadmin:esadmin  -s "http://10.0.3.131:9200/_snapshot/elk_backup/$1/_restore"
+#curl -XPOST -u esadmin:esadmin  -s "http://10.0.3.131:9200/.kibana/_open"
 
 echo "IF YOU HAVE ACCESS_DENIED_EXCEPTION, UNCOMMENT THE SHARED FOLDER LINE IN YOUR VAGRANTFILE!"
